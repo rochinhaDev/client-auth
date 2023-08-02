0c12a6bb3d9eb9fb17c741c3e80c649ef45dd71f
rochinhaDev <you@example.com>
2023-08-01 22:31:22 -0300

    mais

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,76 @@
-
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
-   return ( 
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  function handleChange(e) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/user/login",
+        form
+      );
+      const token = response.data.token;
+      localStorage.setItem("userToken", token);
+      navigate("/profile");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  console.log(form);
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/user/login",
+        form
+      );
+      navigate("/profile");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}></form>
       <div>
-         <h1>Login Page</h1>
-         
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          required={true}
+        />
       </div>
-    );
+      <div>
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+          required={true}
+        />
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Login
+      </button>
+    </div>
+  );
 }
 
 export default LoginPage;
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,11 +1,23 @@
-
-
+import { useState, useEffect } from "react";
+import api from "../axios/api";
 function ProfilePage() {
-   return ( 
-      <div>
-         <h1>Profile Page</h1>
-      </div>
-    );
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    async function getProfile() {
+      try {
+        const reponse = await axios.get("/user/profile");
+        setUser(reponse.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getProfile();
+  }, []);
+  return (
+    <div>
+      <h1>Profile Page</h1>
+    </div>
+  );
 }
 
 export default ProfilePage;
