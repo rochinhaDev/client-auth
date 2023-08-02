7d793cbae6f38b8186232c3768aa0cf6d5e975f2
rochinhaDev <you@example.com>
2023-08-01 21:36:48 -0300

    atualizado

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,10 +1,80 @@
-
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 function SignupPage() {
-   return ( 
+   const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    telefone: "",
+    password: "",
+  });
+  function handleChange(e) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/user/signup",
+        form
+      );
+      navigate("/login");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  console.log(form);
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Signup Page</h1>
       <div>
-         <h1>Signup Page</h1>
+        <label>Nome Completo</label>
+        <input
+          type="text"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+          required={true}
+        />
       </div>
-    );
+      <div>
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          required={true}
+        />
+      </div>
+      <div>
+        <label>Telefone</label>
+        <input
+          type="tel"
+          name="telefone"
+          value={form.telefone}
+          onChange={handleChange}
+          required={true}
+        />
+      </div>
+      <div>
+        <label>Senha</label>
+        <input
+          type="password"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+          required={true}
+        />
+      </div>
+      <button onClick={handleSubmit}>Cadastre-se</button>
+    </form>
+  );
 }
 
 export default SignupPage;
