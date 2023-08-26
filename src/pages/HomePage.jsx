d5d21ddddb4c02bb55aa9454ec22834ab4b065ce
rochinhaDev <you@example.com>
2023-08-26 15:40:45 -0300

    mais css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,7 +15,7 @@ export default function HomePage() {
   console.log(jobs);
   return (
     <main>
-      <div className="relative">
+      <div className="relative mb-4">
         <img
           className="h-32 w-full object-cover rounded-md"
           src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2669&q=80"
@@ -31,11 +31,28 @@ export default function HomePage() {
       <div>{/* AQUI VAI O SEARCH BAR E O FILTRO */}</div>
 
       {/* aqui vai mostrar os cards */}
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
         {jobs.map((job) => {
           return (
-            <div key={job._id}>
-              <h2>{job.title}</h2>
+            <div
+              key={job._id}
+              className="bg-white rounded-lg shadow-sm p-2 ring-1 ring-gray-400 ring-offset-2 transform hover:scale-95 transition-transform duration-300"
+            >
+              <h2 className="text-lg font-bold">{job.title}</h2>
+              <p className="text-xs">Local: {job.city}</p>
+              <div className="flex gap-4 my-2">
+                <p className="bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 rounded-md">
+                  {job.model}
+                </p>
+                <p className="bg-purple-50 px-2 py-1 text-xs font-medium text-indigo-700 rounded-md">
+                  Salário: R${job.salary},00
+                </p>
+              </div>
+              <div className="border-t pt-1">
+                <p className="text-sm font-semibold leading-6 text-gray-800 hover:underline">
+                  Ver detalhes &rarr;
+                </p>
+              </div>
             </div>
           );
         })}
